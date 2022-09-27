a7c2e7de158c3c2e775b3de2d58bed2dcca409f6
johanngerberding <johann.gerberding@gmail.com>
2022-09-27 16:51:56 +0200

    started working on data augmentation

--- detection/voxelnet/dataset.py
+++ detection/voxelnet/dataset.py
@@ -4,8 +4,18 @@ import cv2
 import numpy as np
 import torch 
 from torch.utils import data 
+import math 
 
-from utils import pcl_to_voxels 
+from utils import (
+    pcl_to_voxels, 
+    label_to_gt_box_3d, 
+    camera_to_lidar_box, 
+    center_to_corner_box3d,
+    lidar_to_camera_point,
+    angle_in_limit,
+) 
+
+
 from config import get_cfg_defaults 
 
 cfg = get_cfg_defaults()
@@ -115,13 +125,212 @@ def pcl_augmentation(tag: str):
         cfg.DATA.DIR, 'label_2', tag + '.txt'), 'r').readlines()])
 
     cls_name = np.array([line.split()[0] for line in label])
-    gt_box_3d = label_to_gt_box(
+    gt_box_3d = label_to_gt_box_3d(
         np.array(label)[np.newaxis, :], 
         cls='', 
         coordinate='camera',
     )[0]
 
-    return rgb
+    choice = np.random.randint(0, 10)
+
+    # paper section 3.2
+    if choice >= 7:
+        # bounding box augmentation 
+        lidar_center_gt_box3d = camera_to_lidar_box(gt_box_3d) 
+        lidar_corner_gt_box3d = center_to_corner_box3d(
+                lidar_center_gt_box3d, coordinate='lidar')
+
+        for idx in range(len(lidar_corner_gt_box3d)):
+            is_collision = True 
+            count = 0 
+            while is_collision and count < 100:
+                t_rz = np.random.uniform(-np.pi / 10, np.pi / 10)
+                t_x = np.random.normal()
+                t_y = np.random.normal()
+                t_z = np.random.normal()
+
+                tmp = box_transform(
+                        lidar_center_gt_box3d[[idx]], 
+                        t_x, t_y, t_z, t_rz, 'lidar')
+                is_collision = False 
+
+                for idy in range(idx):
+                    x1, y1, w1, l1, r1 = tmp[0][[0, 1, 4, 5, 6]]
+                    x2, y2, w2, l2, r2 = lidar_center_gt_box3d[idy][[0, 1, 4, 5, 6]]
+                    iou = calc_iou2d(
+                        np.array([x1, y1, w1, l1, r1], dtype=np.float32),
+                        np.array([x2, y2, w2, l2, r2], dtype=np.float32),
+                    )
+                    if iou > 0:
+                        is_collision = True 
+                        count += 1 
+                        break 
+            if not is_collision:
+                box_corner = lidar_corner_gt_box3d[idx]
+                minx = np.min(box_corner[:, 0])
+                miny = np.min(box_corner[:, 1])
+                minz = np.min(box_corner[:, 2])
+                maxx = np.max(box_corner[:, 0])
+                maxy = np.max(box_corner[:, 1])
+                maxz = np.max(box_corner[:, 2])
+                bound_x = np.logical_and(lidar[:, 0] >= minx, lidar[:, 0] <= maxx)
+                bound_y = np.logical_and(lidar[:, 1] >= miny, lidar[:, 1] <= maxy)
+                bound_z = np.logical_and(lidar[:, 2] >= minz, lidar[:, 2] <= maxz)
+                bound_box = np.logical_and(np.logical_and(bound_x, bound_y), bound_z)
+                lidar[bound_box, 0:3] = point_transform(lidar[bound_box, 0:3], t_x, t_y, t_z, t_rz)
+                lidar_center_gt_box3d[idx] = box_transform(
+                        lidar_center_gt_box3d[[idx]], t_x, t_y, t_z, t_rz, 'lidar')
+        gt_box3d = lidar_to_camera_box(lidar_center_gt_box3d)
+        newtag = 'aug_{}_1_{}'.format(tag, np.random.randint(1, 1024))
+    
+    elif choice < 7 and choice >= 4:
+        # global rotation 
+        pass 
+    
+    else:
+        # global scaling
+        pass
+    
+    label = box3d_to_label(gt_box3d[np.newaxis, ...], cls_name[np.newaxis, ...], coordinate='camera')[0]
+    voxel_dict = process_pointcloud()
+    return newtag, rgb, lidar, voxel_dict, label 
+
+
+def calc_iou2d():
+    return None 
+
+
+def box_transform(lidar_center_boxes, t_x, t_y, t_z, t_rz, coordinate='lidar'):
+    boxes_corner = center_to_corner_box3d(
+            lidar_center_boxes, coordinate=coordinate)
+    for idx in range(len(boxes_corner)):
+        boxes_corner[idx] = point_transform(
+                boxes_corner[idx], t_x, t_y, t_z, t_rz) 
+
+    return corner_to_center_box3d(boxes_corner, coordinate=coordinate) 
+
+
+def point_transform(corner_box, tx, ty, tz, rz=0, ry=0, rx=0):
+    N = corner_box.shape[0]
+    points = np.hstack([corner_box, np.ones((N, 1))])
+    
+    # apply translation 
+    mat1 = np.eye(4)
+    mat1[3, 0:3] = tx, ty, tz
+    points = np.matmul(points, mat1)
+    
+    if rx != 0:
+        mat = np.zeros((4, 4)) 
+        mat[0, 0] = 1
+        mat[3, 3] = 1 
+        mat[1, 1] = np.cos(rx) 
+        mat[1, 2] = -np.sin(rx) 
+        mat[2, 1] = np.sin(rx)
+        mat[2, 2] = np.cos(rx)
+        points = np.matmul(points, mat)
+    if ry != 0:
+        mat = np.zeros((4, 4)) 
+        mat[1, 1] = 1
+        mat[3, 3] = 1 
+        mat[0, 0] = np.cos(ry) 
+        mat[0, 2] = np.sin(ry) 
+        mat[2, 0] = -np.sin(ry)
+        mat[2, 2] = np.cos(ry)
+        points = np.matmul(points, mat)
+
+    if rz != 0:
+        mat = np.zeros((4, 4)) 
+        mat[2, 2] = 1
+        mat[3, 3] = 1 
+        mat[0, 0] = np.cos(rz) 
+        mat[0, 1] = -np.sin(rz) 
+        mat[1, 0] = np.sin(rz)
+        mat[1, 1] = np.cos(rz)
+        points = np.matmul(points, mat)
+
+    return points[:, 0:3] 
+
+
+def corner_to_center_box3d(
+    boxes_corner, 
+    coordinate='camera', 
+    T_VELO_2_CAM = None, 
+    R_RECT_0 = None,
+    corner2centeravg = True,
+):
+    # (N, 8, 3) -> (N, 7); x,y,z,h,w,l,ry/z
+    if coordinate == 'lidar':
+        for idx in range(len(boxes_corner)):
+            boxes_corner[idx] = lidar_to_camera_point(
+                    boxes_corner[idx], T_VELO_2_CAM, R_RECT_0)
+    ret = []
+    for roi in boxes_corner:
+        if corner2centeravg:  # average version
+            roi = np.array(roi)
+            h = abs(np.sum(roi[:4, 1] - roi[4:, 1]) / 4)
+            w = np.sum(
+                np.sqrt(np.sum((roi[0, [0, 2]] - roi[3, [0, 2]])**2)) +
+                np.sqrt(np.sum((roi[1, [0, 2]] - roi[2, [0, 2]])**2)) +
+                np.sqrt(np.sum((roi[4, [0, 2]] - roi[7, [0, 2]])**2)) +
+                np.sqrt(np.sum((roi[5, [0, 2]] - roi[6, [0, 2]])**2))
+            ) / 4
+            l = np.sum(
+                np.sqrt(np.sum((roi[0, [0, 2]] - roi[1, [0, 2]])**2)) +
+                np.sqrt(np.sum((roi[2, [0, 2]] - roi[3, [0, 2]])**2)) +
+                np.sqrt(np.sum((roi[4, [0, 2]] - roi[5, [0, 2]])**2)) +
+                np.sqrt(np.sum((roi[6, [0, 2]] - roi[7, [0, 2]])**2))
+            ) / 4
+            x = np.sum(roi[:, 0], axis = 0) / 8
+            y = np.sum(roi[0:4, 1], axis = 0) / 4
+            z = np.sum(roi[:, 2], axis = 0) / 8
+            ry = np.sum(
+                math.atan2(roi[2, 0] - roi[1, 0], roi[2, 2] - roi[1, 2]) +
+                math.atan2(roi[6, 0] - roi[5, 0], roi[6, 2] - roi[5, 2]) +
+                math.atan2(roi[3, 0] - roi[0, 0], roi[3, 2] - roi[0, 2]) +
+                math.atan2(roi[7, 0] - roi[4, 0], roi[7, 2] - roi[4, 2]) +
+                math.atan2(roi[0, 2] - roi[1, 2], roi[1, 0] - roi[0, 0]) +
+                math.atan2(roi[4, 2] - roi[5, 2], roi[5, 0] - roi[4, 0]) +
+                math.atan2(roi[3, 2] - roi[2, 2], roi[2, 0] - roi[3, 0]) +
+                math.atan2(roi[7, 2] - roi[6, 2], roi[6, 0] - roi[7, 0])
+            ) / 8
+            if w > l:
+                w, l = l, w
+                ry = angle_in_limit(ry + np.pi / 2)
+        else:  # max version
+            h = max(abs(roi[:4, 1] - roi[4:, 1]))
+            w = np.max(
+                np.sqrt(np.sum((roi[0, [0, 2]] - roi[3, [0, 2]])**2)) +
+                np.sqrt(np.sum((roi[1, [0, 2]] - roi[2, [0, 2]])**2)) +
+                np.sqrt(np.sum((roi[4, [0, 2]] - roi[7, [0, 2]])**2)) +
+                np.sqrt(np.sum((roi[5, [0, 2]] - roi[6, [0, 2]])**2))
+            )
+            l = np.max(
+                np.sqrt(np.sum((roi[0, [0, 2]] - roi[1, [0, 2]])**2)) +
+                np.sqrt(np.sum((roi[2, [0, 2]] - roi[3, [0, 2]])**2)) +
+                np.sqrt(np.sum((roi[4, [0, 2]] - roi[5, [0, 2]])**2)) +
+                np.sqrt(np.sum((roi[6, [0, 2]] - roi[7, [0, 2]])**2))
+            )
+            x = np.sum(roi[:, 0], axis = 0) / 8
+            y = np.sum(roi[0:4, 1], axis = 0) / 4
+            z = np.sum(roi[:, 2], axis = 0) / 8
+            ry = np.sum(
+                math.atan2(roi[2, 0] - roi[1, 0], roi[2, 2] - roi[1, 2]) +
+                math.atan2(roi[6, 0] - roi[5, 0], roi[6, 2] - roi[5, 2]) +
+                math.atan2(roi[3, 0] - roi[0, 0], roi[3, 2] - roi[0, 2]) +
+                math.atan2(roi[7, 0] - roi[4, 0], roi[7, 2] - roi[4, 2]) +
+                math.atan2(roi[0, 2] - roi[1, 2], roi[1, 0] - roi[0, 0]) +
+                math.atan2(roi[4, 2] - roi[5, 2], roi[5, 0] - roi[4, 0]) +
+                math.atan2(roi[3, 2] - roi[2, 2], roi[2, 0] - roi[3, 0]) +
+                math.atan2(roi[7, 2] - roi[6, 2], roi[6, 0] - roi[7, 0])
+            ) / 8
+            if w > l:
+                w, l = l, w
+                ry = angle_in_limit(ry + np.pi / 2)
+        ret.append([x, y, z, h, w, l, ry])
+    if coordinate == 'lidar':
+        ret = camera_to_lidar_box(np.array(ret), T_VELO_2_CAM, R_RECT_0)
+
+    return np.array(ret)
 
 
 def test():
